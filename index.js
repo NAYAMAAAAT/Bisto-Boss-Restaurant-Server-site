7c66e4d896b23b6d7ba53662906c283b8494297c
NAYAMAAAAT <atefmdnayamatullah@gmail.com>
2025-05-03 05:56:27 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.port || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+app.use(cors());
+app.use(express.json());
+
+const uri = "mongodb://bossUser:DB_PASS@cluster0-shard-00-00.dtwqg.mongodb.net:27017,cluster0-shard-00-01.dtwqg.mongodb.net:27017,cluster0-shard-00-02.dtwqg.mongodb.net:27017/?ssl=true&replicaSet=atlas-woksge-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+
+    
+
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+
+
+
+
+
+app.get('/',(req,res)=> {
+    res.send('boSS is cooking')
+})
+
+
+app.listen(port,() => {
+    console.log(`server on port: ${port}`)
+})
