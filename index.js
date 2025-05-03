47a952486f51ac5db90e0431f6d9372578f6e4c1
NAYAMAAAAT <atefmdnayamatullah@gmail.com>
2025-05-03 19:07:09 +0600

    Add cart data wit client

--- index.js
+++ index.js
@@ -20,12 +20,36 @@ const client = new MongoClient(uri, {
     try {
       // Connect the client to the server	(optional starting in v4.7)
       const MenuCollection = client.db('bistroDb').collection('menu');
-      // const ReviewCollection = client.db('bistroDb').collection('reviews');
+      const ReviewCollection = client.db('bistroDb').collection('reviews')
+      const CartCollection = client.db('bistroDb').collection('carts')
     
       app.get('/menu', async (req, res) => {
         const result = await MenuCollection.find().toArray()
         res.send(result)
     })
+    app.get('/review', async (req, res) => {
+      const result = await ReviewCollection.find().toArray()
+      res.send(result)
+  })
+  app.post('/carts', async (req, res) => {
+      const cartInfo = req.body;
+      const result = await CartCollection.insertOne(cartInfo);
+      res.send(result);
+  })
+  app.get('/carts', async (req, res) => {
+    const email = req.query.email;
+    const query = {
+        userEmail: email
+    }
+    const result = await CartCollection.find(query).toArray()
+    res.send(result)
+})
+app.delete('/carts/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const result = await CartCollection.deleteOne(query)
+    res.send(result)
+})
       await client.connect();
       // Send a ping to confirm a successful connection
       await client.db("admin").command({ ping: 1 });
